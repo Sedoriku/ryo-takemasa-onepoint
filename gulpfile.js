8900a5d1fc218ddfa7a0eb7a91b34259bbb762ae
Cédric Guimet <cedric.guimet@cguimet.fr>
2018-02-08 19:45:15 +0100

    - updated gulpfile - started html template/structure

--- gulpfile.js
+++ gulpfile.js
@@ -5,64 +5,40 @@ var rename = require('gulp-rename');
 // Include plugins
 var plugins = require('gulp-load-plugins')(); // tous les plugins de package.json
 
-// Variables de chemins
-var source = 'wp-content/themes/sweetcloset'; // dossier de travail
-var destination = 'wp-content/themes/sweetcloset'; // dossier à livrer
 
 // Tâche "build" = LESS + autoprefixer + CSScomb + beautify (source -> destination)
 gulp.task('css', function () {
-  return gulp.src(source + '/scss/*.scss')
+  return gulp.src('scss/*.scss')
     .pipe(plugins.sass())
     .pipe(plugins.csscomb())
     .pipe(plugins.cssbeautify({indent: '  '}))
     .pipe(plugins.autoprefixer())
-    .pipe(plugins.replace('/img/', '/wp-content/themes/sweetcloset/img/'))
-    .pipe(gulp.dest(destination + '/css/'));
+    .pipe(gulp.dest('css/'));
 });
 
-gulp.task('js', function () {
-    return gulp.src(source + '/js/*.js')
+gulp.task('jsmin', function () {
+    return gulp.src('js/*.js')
         .pipe(plugins.jsmin())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(destination + '/dist/js/'));
+        .pipe(gulp.dest('js/'));
 });
 
 // Tâche "minify" = minification CSS (destination -> destination)
 gulp.task('minify', function () {
-  return gulp.src(source + '/css/*.css')
+  return gulp.src('css/*.css')
     .pipe(plugins.csso())
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest(destination + '/dist/css/'));
+    .pipe(gulp.dest('css/'));
 });
-
-var mysqlDump = require('mysqldump');
-
-//tache dump_db
-gulp.task('mysqldump', function() {
-
-    mysqlDump({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'wp_sweetcloset',
-        dest:'./wp_sweetcloset.sql' // destination file
-    },function(err){
-        // create data.sql file;
-    })
-});
-
 
 // Tâche "build"
 gulp.task('build', ['css']);
 
-// Tâche "prod" = Build + minify
-gulp.task('prod', ['build',  'minify', 'js']);
-
 // Tâche "watch" = je surveille *less
 gulp.task('watch', function () {
-  gulp.watch(source + '/scss/*.scss', ['build']);
+  gulp.watch('scss/*.scss', ['build']);
 });
 
 // Tâche par défaut
