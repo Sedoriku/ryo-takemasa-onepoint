825547782f903d764d0530c2393149080189589c
Cédric Guimet <cedric.guimet@cguimet.fr>
2018-02-09 18:22:10 +0100

    Added masonry and image loaded support

--- js/main.js
+++ js/main.js
@@ -1,39 +1,57 @@
+var masonry_grid;
+var $gridItem
 $(document).ready(function() {
+
+    masonryInit();
 
     $.getJSON('https://jsonplaceholder.typicode.com/posts', function(data) {
         $.each(data, function(i) {
-
-            if (i < 25) {
+            if (i < 15) {
                 // get a random number
                 // it is added to url for random image
                 // all images will be the same without the id (https://github.com/unsplash/unsplash-source-js/issues/9)
                 randomImgId = getRandomInt(1000);
 
+                $gridItem =
+                    $('<div class="grid-item js-grid-item grid-item--image">'+
+                        '<div class="item-hover_border"></div>'+
+                        '<img src="https://source.unsplash.com/random?sig='+randomImgId+'" alt="" class="item-img">'+
+                        '<div class="item-title_wrapper"> '+
+                            '<h3 class="item-title item-title--medium">'+ data[i].title +'</h3>'+
+                        '</div>'+
+                    '</div>');
 
-                var grid_post_html = '<div class="grid-item js-grid-item grid-item--image">';
-                    grid_post_html +=   '<div class="item-hover_border"></div>';
-                    grid_post_html +=   '<img src="https://source.unsplash.com/random?sig='+randomImgId+'" alt="" class="item-img">';
-                    grid_post_html +=   '<div class="item-title_wrapper"> ';
-                    grid_post_html +=       '<h3 class="item-title item-title--medium">'+ data[i].title +'</h3>';
-                    grid_post_html +=   '</div>';
-                    grid_post_html +='</div>';
-
-                $('.js-grid').append(grid_post_html);
             }
+            masonry_grid.append($gridItem).masonry('appended', $gridItem);
+
         });
+    }).then(function() {
+        imagesloaded();
     });
 
-
-    //remove node #text from html
+    //remove unecessary node #text from html
     clean(document.body);
 
-    // $('.js-grid').masonry({
-    //     itemSelector: '.grid-item',
-    //     columnWidth: 300
-    // });
+
 
 });
 
+
+function masonryInit() {
+    masonry_grid = $('.js-grid').masonry({
+        itemSelector: '.js-grid-item',
+        columnWidth: 300,
+        gutter : 10
+    });
+
+}
+
+function imagesloaded() {
+    $('.js-grid-item').imagesLoaded().progress(function() {
+        console.log("jfkldjdlsm");
+        masonry_grid.masonry('layout');
+    });
+}
 
 
 function getRandomInt(max) {
