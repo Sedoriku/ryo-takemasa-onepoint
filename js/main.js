254335bfff3d97003dee5f6cb8147ac19f51f411
Cédric Guimet <cedric.guimet@cguimet.fr>
2018-02-09 21:20:16 +0100

    Added item flip

--- js/main.js
+++ js/main.js
@@ -6,23 +6,30 @@ $(document).ready(function() {
 
     $.getJSON('https://jsonplaceholder.typicode.com/posts', function(data) {
         $.each(data, function(i) {
-            if (i < 15) {
+            if (i < 16) {
                 // get a random number
                 // it is added to url for random image
                 // all images will be the same without the id (https://github.com/unsplash/unsplash-source-js/issues/9)
-                randomImgId = getRandomInt(1000);
-
+                randomImgId = getRandomInt(4000);
+                console.log(randomImgId);
                 $gridItem =
-                    $('<div class="grid-item js-grid-item grid-item--image">'+
+                    $('<div class="grid-item js-grid-item js-grid-item--flippable grid-item--image">'+
                         '<div class="item-hover_border"></div>'+
-                        '<img src="https://source.unsplash.com/random?sig='+randomImgId+'" alt="" class="item-img">'+
-                        '<div class="item-title_wrapper"> '+
-                            '<h3 class="item-title item-title--medium">'+ data[i].title +'</h3>'+
+                        '<div class="item-flipper">'+
+                            '<div class="item-front">'+
+                                '<img src="https://source.unsplash.com/random?sig='+randomImgId+'" alt="" class="item-img">'+
+                                '<div class="item-title_wrapper"> '+
+                                    '<h3 class="item-title item-title--medium">'+ data[i].title +'</h3>'+
+                                '</div>'+
+                            '</div>'+
+                            '<div class="item-back">'+
+                                '<p class="back-txt">'+data[i].body+'</p>'+
+                            '</div>'+
                         '</div>'+
                     '</div>');
 
+                    masonry_grid.append($gridItem).masonry('appended', $gridItem);
             }
-            masonry_grid.append($gridItem).masonry('appended', $gridItem);
 
         });
     }).then(function() {
@@ -32,6 +39,13 @@ $(document).ready(function() {
     //remove unecessary node #text from html
     clean(document.body);
 
+    $(window).resize(function() {
+        masonry_grid.masonry('layout');
+    });
+
+    $(document).on('click', '.js-grid-item--flippable', function() {
+        $(this).toggleClass('grid-item--flipped');
+    });
 
 
 });
@@ -43,12 +57,10 @@ function masonryInit() {
         columnWidth: 300,
         gutter : 10
     });
-
 }
 
 function imagesloaded() {
     $('.js-grid-item').imagesLoaded().progress(function() {
-        console.log("jfkldjdlsm");
         masonry_grid.masonry('layout');
     });
 }
